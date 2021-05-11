6506eb7457dfe1b2e9a664a574ba1a2685baa87c
ygw <ygw1011@gmail.com>
2021-05-11 17:03:46 +0900

    write server

--- hello.js
+++ hello.js
@@ -1,4 +1,12 @@
-console.log("hi");
-for(var i = 0; i < 10; i++){
-    console.log("hi " + i)
-}
+var http = require('http'); 
+
+var server = http.createServer(function(request,response){ 
+
+    response.writeHead(200,{'Content-Type':'text/html'});
+    response.end('Hello node.js!!');
+
+});
+
+server.listen(9045, function(){ 
+    console.log('Server is running...');
+});
